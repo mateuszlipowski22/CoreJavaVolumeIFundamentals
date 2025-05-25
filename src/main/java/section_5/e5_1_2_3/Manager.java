0ee050b73c59b5ec90a3efc6d24ec58ff309e2b9
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2025-05-25 22:49:00 +0200

    e5_1_2_3

---
+++ src/main/java/section_5/e5_1_2_3/Manager.java
@@ -0,0 +1,19 @@
+package section_5.e5_1_2_3;
+
+public class Manager extends Employee{
+
+    private double bonus;
+
+    public Manager(String name, double salary, int year, int month, int day, double bonus) {
+        super(name, salary, year, month, day);
+        this.bonus = bonus;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+}
